b058a5cb1963057ba1f5b4346153b99320175894
Yol-B <yoltorres24@gmail.com>
2019-11-05 08:51:37 +0800

    initial

--- src/components/developers.vue
+++ src/components/developers.vue
@@ -6,6 +6,7 @@
       </v-col>
       <v-col cols='10' class="text-left">
         <h1>{{name}}</h1>
+          <p>{{description}}</p>
       </v-col>
     </v-row>
   </v-card>
@@ -14,7 +15,14 @@
 export default {
   props: {
     name: String,
+    description: String,
     picture: String
   }
 };
 </script>
+
+<style scoped>
+  v-row{
+    background-color:aqua;
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="about">
-    <dev v-bind:picture = "require('@/assets/princess.png')" v-bind:name="'Princess'"></dev>
-    <dev v-bind:picture = "require('@/assets/lorly.png')" v-bind:name="'Lorly'"></dev>
-    <dev v-bind:picture = "require('@/assets/mellyne.png')" v-bind:name="'Mellyne'"></dev>
-    <dev v-bind:picture = "require('@/assets/jacky.png')" v-bind:name="'Jakeniel'"></dev>
+    <dev v-bind:picture = "require('@/assets/princess.png')" v-bind:name="'Princess Joy Duran'"  v-bind:description="'Project Manager'"></dev>
+    <dev v-bind:picture = "require('@/assets/lorly.png')" v-bind:name="'Lorly S. Sugal'" v-bind:description="'Project Developer'"></dev>
+    <dev v-bind:picture = "require('@/assets/mellyne.png')" v-bind:name="'Mellyne Grace Nadela'" v-bind:description="'Project Developer'"></dev>
+    <dev v-bind:picture = "require('@/assets/jacky.png')" v-bind:name="'Jakeniel Erim'" v-bind:description="'Project Developer'"></dev>
   </div>
-
-
-  </template>
+</template>
 
 <script>
 import dev from "@/components/developers";
@@ -17,6 +15,10 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+</style>
 
 
 
